43ce4eea8106cfe9896307eed1ffd4c7423d1552
Sahil Singh Yadav <91723844+cptSahil@users.noreply.github.com>
2022-04-10 02:44:28 +0530

    Create FInd All Duplicates in an array.py

---
+++ Day 8/FInd All Duplicates in an array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        l = []
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                l.append(nums[i])
+            else:
+                continue
+        return l
